d1f2c3690aa8cd4d28ae34e90392897ef0bcec43
BoGyX <166905990+BoGyX@users.noreply.github.com>
2024-04-13 12:02:57 +0300

    Add files via upload

---
+++ DAMAGE666/ItalianoMenu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DAMAGE666.IL_PATIODataSetTableAdapters;
+namespace DAMAGE666
+{
+    public partial class ItalianoMenu : Page
+    {
+        ITALIAN_MENU_PATIOTableAdapter italia = new ITALIAN_MENU_PATIOTableAdapter();
+        public ItalianoMenu()
+        {
+            InitializeComponent();
+            ColourDataGrid.ItemsSource = italia.GetData();
+        }
+    }
+}
+      
---
+++ DAMAGE666/JapaneseMenu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DAMAGE666.IL_PATIODataSetTableAdapters;
+namespace DAMAGE666
+{
+    public partial class JapaneseMenu : Page
+    {
+        JAPANESE_MENU_PATIOTableAdapter japan = new JAPANESE_MENU_PATIOTableAdapter();
+        public JapaneseMenu()
+        {
+            InitializeComponent();
+            ColourDataGrid.ItemsSource = japan.GetData();
+
+        }
+    }
+}
